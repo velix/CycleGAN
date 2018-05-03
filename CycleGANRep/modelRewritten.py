d68edd3218163825666a9d72af2239aa63c98bf0
Oguzhanu5 <38907208+Oguzhanu5@users.noreply.github.com>
2018-05-03 16:28:02 +0200

    Add files via upload

--- CycleGANRep/modelRewritten.py
+++ CycleGANRep/modelRewritten.py
@@ -33,55 +33,56 @@ def build_resnet_block(inputres, dim, name="resnet"):
         return tf.nn.relu(out_res + inputres)
 
 
-#numberOfGEN: the number of Cnn:s in the Encoder
-#numberOfres: is the number of resNets
-#numberOfDec: is the number of deconvolutional layers in the decoder
-#reflection = 'true': sets the reflaction padding as default but you can choose to to set it false
-#imageDim: is the image dimension which is used on the decoding part
+'''numberOfGEN: the number of Cnn:s in the Encoder
+numberOfres: is the number of resNets
+numberOfDec: is the number of deconvolutional layers in the decoder
+reflection = 'true': sets the reflaction padding as default but you can choose to to set it false
+imageDim: is the image dimension which is used on the decoding part'''
+
 def BuildGeneratorAndResNet(inputgen,numberOfGEN, numberOfres,numberOfDec,imageDim,reflection = 'true' , name="generator"):
     with tf.variable_scope(name):
         f = 7
         ks = 3
 
-        # These variables are used in the decoder where dimfromRes is the output
-        # from the resNet into the decoder
-        # ordDim is the ordinary dimension
+        '''These variables are used in the docder where dimfromRes is the output
+         from the resNet into the decoder
+         ordDim is the ordinary dimension'''
         dimFromRes = [batch_size, imageDim/2, imageDim/2, ngf * 2]
         ordDim = [batch_size, imageDim, imageDim, ngf]
 
-        #Creates Encoder
-        #This part creates as many cnn:s as the numberOfGEN in the input of the function
+        '''Creates Encoder
+        This part creates as many cnn:s as the numberOfGEN in the input of the function'''
         pad_input = tf.pad(inputgen, [[0, 0], [ks, ks], [ks, ks], [0, 0]], "REFLECT")
         for i in range(numberOfGEN):
-            #This if statement initializes the first cnn and adds it to the second
-            # and when i is larger then 1 it multiplies ngf with 2 for each new layer and changes the name
+            '''This if statement initializes the first cnn and adds it to the second
+             and when i is larger then 1 it multiplies ngf with 2 for each new layer and changes the name'''
             if i == 0:
                 outputG = general_conv2d(pad_input, ngf, f, f, 1, 1, 0.02, name="c"+str(i+1))
             else:
                 outputG = general_conv2d(outputG, ngf * (i*2), ks, ks, 2, 2, 0.02, "SAME", "c" + str(i+1))
 
-        #Creates ResNet
-        #This for loop creates as many resnets as numberOfres
+        '''Creates ResNet
+        This for loop creates as many resnets as numberOfres'''
         for r in range(1,numberOfres+1):
-            # This if statement initializes the first resnet and sends it in to
-            # the second and so on
+            ''' This if statement initializes the first resnet and sends it in to
+             the second and so on'''
             if r == 1:
                 outputR = build_resnet_block(outputG, ngf * 4, "r"+str(r))
             else:
                 outputR = build_resnet_block(outputR, ngf * 4, "r"+str(r))
 
-        #Creates Decoder
-        #Creates as many deconvolutional layers as the numberOfDec
+        '''Creates Decoder
+        Creates as many deconvolutional layers as the numberOfDec'''
         for k in range(1,numberOfDec+1):
-            # this if statement initializes the first deconvolutional layer that
-            # takes the input from the resnets
+        ''' this if statement initializes the first deconvolutional layer that
+             takes the input from the resnets'''
             if k ==1:
                 outputD = general_deconv2d(outputR, dimFromRes, ngf * 2, ks, ks, 2, 2, 0.02, "SAME", "c"+str(i+k))
             else:
-                # The layers between the first and the second last has zero padding(else statement)
-                # while the second last has a reflection padding for the code with 6 resnets
-                # we can basically choose to set the the reflection padding to false if we don't want one
-                # The last deconvolutional layer doesn't have any padding and represents the elif statement
+                 '''The layers between the first and the second last has zero padding(else statement)
+                 while the second last has a reflection padding for the code with 6 resnets
+                 we can basically choose to set the the reflection padding to false if we don't want one
+                 The last deconvolutional layer doesn't have any padding and represents the elif statement'''
                 if k == numberOfDec-1 and reflection == 'true':
                     outputD = tf.pad(outputD, [[0, 0], [ks, ks], [ks, ks], [0, 0]], "REFLECT")
                 elif k == numberOfDec:
@@ -96,19 +97,19 @@ def BuildGeneratorAndResNet(inputgen,numberOfGEN, numberOfres,numberOfDec,imageD
         return outGenerator
 
 
-# This function creates the discriminator
-# We can choose to use a patch discrimiantor or an ordinary discrimiantor
-# by setting patch to false or true respectively
+''' This function creates the discriminator
+    We can choose to use a patch discrimiantor or an ordinary discrimiantor
+    by setting patch to false or true respectively'''
 def buildDiscriminator(inputdisc,numberOfDisc, patch = 'false',name="discriminator"):
     with tf.variable_scope(name):
         f = 4
-        # This if statement initializes the first convolutional layer depending on whether
-        # the patch is true or false, if it is true then we use crop which
-        # randomly crops a tensor to a given size ([1,70,70,3] (elif statement))
-        # If patch is false then we use a normal convolutional layer (if statement),
-        # the ndf for the layer increases with two between the first and the second layer (else)
-        # The first and the last layer doesn't have relu or norm
-        
+        ''' This if statement initializes the first convolutional layer depending on whether
+            the patch is true or false, if it is true then we use crop which
+            randomly crops a tensor to a given size ([1,70,70,3] (elif statement))
+            If patch is false then we use a normal convolutional layer (if statement),
+            the ndf for the layer increases with two between the first and the second layer (else)
+            The first and the last layer doesn't have relu or norm'''
+
         for i in range(numberOfDisc):
             if i == 0 and patch == 'false':
                 outputdisc = general_conv2d(inputdisc, ndf, f, f, 2, 2, 0.02, "SAME", "c"+str(i+1), do_norm=False, relufactor=0.2)
@@ -120,6 +121,3 @@ def buildDiscriminator(inputdisc,numberOfDisc, patch = 'false',name="discriminat
                 outputdisc = general_conv2d(outputdisc, ndf *(i*2), f, f, 2, 2, 0.02, "SAME", "c"+str(i+1), relufactor=0.2)
 
         return outputdisc
-
-
-
