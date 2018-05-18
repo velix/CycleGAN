9d994663cdad2a39fafded0489063c92c8bafd1e
velix <miloulis.v@gmail.com>
2018-05-18 13:48:15 +0200

    experiment 9

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ to_test = False
 to_restore = False
 output_path = "./output"
 check_dir = "./output/checkpoints/"
-summary_dir = "./output/2/exp_8"
+summary_dir = "./output/2/exp_9"
 batch_size = 1
 pool_size = 50
 max_images = 100
@@ -180,14 +180,14 @@ class CycleGAN:
         fake_A_recognition_loss = tf.reduce_mean(tf.square(
                                                     self.fake_pool_rec_A))
 
-        disc_A_full_loss = (fake_A_recognition_loss + real_A_recognition_loss)/2.0
+        disc_A_full_loss = (fake_A_recognition_loss + real_A_recognition_loss)/4.0
 
         real_B_recognition_loss = tf.reduce_mean(tf.squared_difference(
                                                     self.rec_B, 1))
         fake_B_recognition_loss = tf.reduce_mean(tf.square(
                                                     self.fake_pool_rec_B))
 
-        disc_B_full_loss = (fake_B_recognition_loss + real_B_recognition_loss)/2.0
+        disc_B_full_loss = (fake_B_recognition_loss + real_B_recognition_loss)/4.0
 
         optimizer = tf.train.AdamOptimizer(self.lr, beta1=0.5)
         # Returns all variables created with trainable=True
