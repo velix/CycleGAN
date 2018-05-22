c9556f546c50f51edceac33e1f46a6e779c7eae3
velix <miloulis.v@gmail.com>
2018-05-22 18:46:31 +0200

    experiment 24 code

--- src/model.py
+++ src/model.py
@@ -53,54 +53,13 @@ def build_resnet_block(inputres, dim, name="resnet"):
 
         return tf.nn.relu(out_res + inputres)
 
-# def build_generator_resnet_1block(inputgen, name="generator"):
-#     with tf.variable_scope(name):
-#         big_kernel = 5
-#         small_kernel = 2
-#         stride = 1
-
-#         # This pads the inputgen with 0
-#         # inputgen is 4D, so the padding arguement needs 4 dimensions
-#         # inputgen = [batch_size, img_width, img_height, img_layerA]
-#         # [0, 0] : pad this dimension of input with no padding before, or after
-#         # [small_kernel, small_kernel]: pad this dimesnion of input with small_kernel values before and after
-#         pad_input = tf.pad(inputgen, [[0, 0], [small_kernel, small_kernel], [small_kernel, small_kernel], [0, 0]],
-#                            "REFLECT")
-
-#         o_c1 = general_conv2d(pad_input, generator_first_layer_filters,
-#                               kernel=big_kernel, stride=1, stddev=0.02,
-#                               name="c1")
-
-#         o_c2 = general_conv2d(o_c1, generator_first_layer_filters*2, kernel=small_kernel,
-#                               stride=stride, stddev=0.02,
-#                               padding="SAME", name="c2")
-
-#         o_r1 = build_resnet_block(o_c2, generator_first_layer_filters*2, "r1")
-
-#         o_c4 = general_deconv2d(o_r1, generator_first_layer_filters, kernel=small_kernel,
-#                                 stride=stride, stddev=0.02, padding="SAME",
-#                                 name="c4")
-
-#         o_c4_pad = tf.pad(o_c4, [[0, 0], [small_kernel, small_kernel],
-#                           [small_kernel, small_kernel], [0, 0]], "REFLECT")
-
-#         o_c6 = general_conv2d(o_c4_pad, img_layer, kernel=big_kernel,
-#                               stride=1, stddev=0.02, padding="VALID",
-#                               name="c6", do_relu=False)
-
-#         # Adding the tanh layer
-
-#         out_gen = tf.nn.tanh(o_c6, "t1")
-
-#         return out_gen
-
-
 # This builds a generator with 2 resnet blocks
 def build_generator_resnet_2blocks(inputgen, name="generator"):
     with tf.variable_scope(name):
-        big_kernel = 4
-        small_kernel = 2
+        big_kernel = 5
+        small_kernel = 3
         stride = 2
+        reflection_padding = 2
 
         print("inputgen shape: ", inputgen.get_shape())
 
@@ -109,69 +68,66 @@ def build_generator_resnet_2blocks(inputgen, name="generator"):
         # inputgen = [batch_size, img_width, img_height, img_layerA]
         # [0, 0] : pad this dimension of input with no padding before, or after
         # [small_kernel, small_kernel]: pad this dimesnion of input with small_kernel values before and after
-        # pad_input = tf.pad(inputgen,
-        #                    [[0, 0], [small_kernel, small_kernel],
-        #                    [small_kernel, small_kernel], [0, 0]],
-        #                    "REFLECT")
+        pad_input = tf.pad(inputgen,
+                           [[0, 0], [reflection_padding, reflection_padding],
+                           [reflection_padding, reflection_padding], [0, 0]],
+                           "REFLECT")
 
-        # print("pad_input shape: ", pad_input.get_shape())
+        print("pad_input shape: ", pad_input.get_shape())
 
         # Encoding
-        o_c1 = general_conv2d(inputgen, generator_first_layer_filters,
+        c7s1_c1 = general_conv2d(pad_input, generator_first_layer_filters,
                               kernel=big_kernel,
-                              stride=stride, 
+                              stride=1, 
                               stddev=0.02,
-                              name="c1")
-        # o_c1.shape = (16, 16, 64)
-        print("o_c1 shape: ", o_c1.get_shape())
+                              name="c7s1_c1")
+        print("c7s1_c1 shape: ", c7s1_c1.get_shape())
 
 
-        o_c2 = general_conv2d(o_c1, generator_first_layer_filters*2,
-                              kernel=big_kernel,
+        d_c2 = general_conv2d(c7s1_c1, generator_first_layer_filters*2,
+                              kernel=small_kernel,
                               stride=stride,
                               stddev=0.02,
-                              padding="SAME", name="c2")
-        # o_c2.shape = (8, 8, 128)
-        print("o_c2 shape: ", o_c2.get_shape())
+                              padding="SAME", name="d_c2")
+        print("d_c2 shape: ", d_c2.get_shape())
 
         # Transformation
-        o_r1 = build_resnet_block(o_c2, generator_first_layer_filters*2, "r1")
+        o_r1 = build_resnet_block(d_c2, generator_first_layer_filters*2, "r1")
         print("o_r1 shape: ", o_r1.get_shape())
         o_r2 = build_resnet_block(o_r1, generator_first_layer_filters*2, "r2")
-        # o_r2.shape = (28, 28, 16)
         print("o_r2 shape: ", o_r2.get_shape())
 
         # Decoding
-        o_c4 = general_deconv2d(o_r2, generator_first_layer_filters,
+        u_c4 = general_deconv2d(o_r2, generator_first_layer_filters,
                                 kernel=small_kernel,
                                 stride=stride, 
                                 stddev=0.02,
-                                padding="SAME", name="c4")
-        print("o_c4 shape: ", o_c4.get_shape())
+                                padding="SAME", name="u_c4")
+        print("u_c4 shape: ", u_c4.get_shape())
 
-        o_c5 = general_deconv2d(o_c4, generator_first_layer_filters,
+        u_c5 = general_deconv2d(u_c4, generator_first_layer_filters,
                                 kernel=small_kernel,
                                 stride=stride, 
                                 stddev=0.02,
-                                padding="SAME", name="c5")
-        print("o_c5 shape: ", o_c5.get_shape())
+                                padding="SAME", name="u_c5")
+        print("u_c5 shape: ", u_c5.get_shape())
 
-        # o_c5_pad = tf.pad(o_c5,
-        #                   [[0, 0], [small_kernel, small_kernel],
-        #                   [small_kernel, small_kernel], [0, 0]],
-        #                   "REFLECT")
-        # print("o_c5 pad shape: ", o_c5_pad.get_shape())
+        u_c5_pad = tf.pad(u_c5,
+                          [[0, 0], [reflection_padding, reflection_padding],
+                          [reflection_padding, reflection_padding], [0, 0]],
+                          "REFLECT")
+        print("u_c5_pad pad shape: ", u_c5_pad.get_shape())
 
-        o_c6 = general_conv2d(o_c5, img_layer,
-                              kernel=1,
-                              stride=1,
+        c7s1_c6 = general_conv2d(u_c5_pad, img_layer,
+                              kernel=big_kernel,
+                              stride=2,
                               stddev=0.02,
-                              padding="VALID", name="c6", do_relu=False)
-        print("o_c6 shape: ", o_c6.get_shape())
+                              padding="VALID", name="c7s1_c6", do_relu=False)
+        print("c7s1_c6 shape: ", c7s1_c6.get_shape())
         print()
-        # Adding the tanh layer
 
-        out_gen = tf.nn.tanh(o_c6, "t1")
+        # Adding the tanh layer
+        out_gen = tf.nn.tanh(c7s1_c6, "t1")
 
         return out_gen
 
