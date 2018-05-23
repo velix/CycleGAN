37e0afe63296968b00053792edb7341749e05d97
velix <miloulis.v@gmail.com>
2018-05-23 16:21:14 +0200

    experiment 26 code

--- src/main.py
+++ src/main.py
@@ -363,7 +363,7 @@ class CycleGAN:
                 if epoch == 0 or epoch % 10 == 0:
                     self._store_image_summaries(writer, sess, epoch)
 
-                sess.run(tf.assign(increase_global_step))
+                sess.run(increase_global_step)
 
     def _store_image_summaries(self, writer, sess, epoch, ptr=99):
 
